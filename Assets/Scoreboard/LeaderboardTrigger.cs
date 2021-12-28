c8e3f77321d0e66814799ad7780fa8dd61269917
blankspeedster <ronie.bituin@trainocate.com>
2021-12-28 13:00:33 +0800

    :pencil: Leaderboard per subject

--- Assets/Scoreboard/LeaderboardTrigger.cs
+++ Assets/Scoreboard/LeaderboardTrigger.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 public class LeaderboardTrigger : MonoBehaviour
 {
 
     public GameObject scoreboardCanvas;
     public GameObject inputCanvas;
+    public Text leaderBoardText;
+    public int subjectId;
     // Start is called before the first frame update
     void Awake()
     {
@@ -20,6 +25,7 @@ public class LeaderboardTrigger : MonoBehaviour
         {
             inputCanvas.gameObject.SetActive(false);
             scoreboardCanvas.gameObject.SetActive(true);
+            LoadScoreBoard();
         }
     }
 
@@ -27,6 +33,41 @@ public class LeaderboardTrigger : MonoBehaviour
     {
         scoreboardCanvas.gameObject.SetActive(false);
         inputCanvas.gameObject.SetActive(true);
+    }
+
+    async void LoadScoreBoard(){
+        string baseURL = PlayerPrefs.GetString("url");
+        int _userId =  PlayerPrefs.GetInt("user_id");
+
+        string message = null;
+
+        string url = $"{baseURL}/process_section.php?score_board={subjectId}&userId={_userId}";
+
+        var client = new HttpClient();
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri(url),
+            Headers =
+            {
+                { "Accept", "application/json" },
+            },
+            Content = new StringContent("")
+            {
+                Headers =
+                {
+                    ContentType = new MediaTypeHeaderValue("application/json")
+                }
+            }
+        };
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            message = body;   
+        }
+
+        leaderBoardText.text = message;
     }
 
    
